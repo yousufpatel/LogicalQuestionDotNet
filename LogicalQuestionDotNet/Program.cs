e01c0deca63c2bc68831833856a7b96ef07cb4fe
yousuf patel <yousuf.glb@gmail.com>
2020-07-12 18:55:44 +0530

    Add files

---
+++ LogicalQuestionDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalQuestionDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortArray.SortArrayElements();
+        }
+    }
+}
---
+++ LogicalQuestionDotNet/SortArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalQuestionDotNet
+{
+    public class SortArray
+    {
+        public static void SortArrayElements()
+        {
+            int[] MyArray = new int[] { 5, 9, 1, 4 };
+
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                for (int j = 0; j < MyArray.Length - 1; j++)
+                {
+                    if (MyArray[j] > MyArray[j + 1])
+                    {
+                        int temp = MyArray[j];
+                        MyArray[j] = MyArray[j + 1];
+                        MyArray[j + 1] = temp;
+                    }
+                }
+
+            }
+
+            for (int k =0; k< MyArray.Length; k++)
+            {
+                Console.WriteLine(MyArray[k]);
+            }
+
+        }
+    }
+}
